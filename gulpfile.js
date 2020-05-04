bc5936a0524a8485166fc5d1f419772bd3b9c6ed
Vital Lazouski <neonvis@gmail.com>
2020-05-04 13:09:39 +0300

    made mincss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -9,4 +10,11 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  });
+
+  gulp.task('mincss', function() {
+    return gulp.src("./*.css")
+    .pipe(rename({suffix: ".min"}))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("app/css"));
+    });
