53850d4875ab2bb2d07bb02e59412fa44435d045
Vital Lazouski <neonvis@gmail.com>
2020-05-20 19:48:01 +0300

    feat: add yandex map

--- js/main.js
+++ js/main.js
@@ -202,6 +202,39 @@ $(document).ready(function () {
   // маска для тел
   $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7(___)___-__-__"});
 
+  // создание yandex карты
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/location.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+    myMap.behaviors.disable('scrollZoom');
+    myMap.geoObjects
+        .add(myPlacemark);
+  }); 
 });
 
  
